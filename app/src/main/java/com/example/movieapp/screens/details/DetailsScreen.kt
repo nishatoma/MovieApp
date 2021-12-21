0ab2bcb741a7d1880c4e71424fe7c05f5a0bb0b0
nishatoma <nishatoma@gmail.com>
2021-12-20 21:13:02 -0500

    add some mods

--- app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
@@ -1,11 +1,28 @@
 package com.example.movieapp.screens.details
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
 
 @Composable
 fun DetailsScreen(navController: NavController, movieData: String?) {
-    // for now just use text
-    Text(text = movieData.toString())
+    Surface(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight()
+    ) {
+        Column(horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center) {
+            Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
+        }
+    }
+
 }

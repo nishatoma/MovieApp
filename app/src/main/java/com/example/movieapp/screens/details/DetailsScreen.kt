eb5315851ac755aeb8cab60cc5f1143d77056a58
nishatoma <nishatoma@gmail.com>
2021-12-20 21:24:23 -0500

    add navigation back button

--- app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
@@ -1,15 +1,14 @@
 package com.example.movieapp.screens.details
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 
 @Composable
@@ -22,6 +21,17 @@ fun DetailsScreen(navController: NavController, movieData: String?) {
         Column(horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center) {
             Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
+            Spacer(modifier = Modifier.height(23.dp))
+            // Button to take us to home screen
+            Button(onClick = {
+                // This is why we pass the nav controller all along!
+                // That's the beauty, of the backstack!
+                // We just have to pop this activity to go back to the previous
+                // screen.
+                navController.popBackStack()
+            }) {
+                Text(text = "Go Back")
+            }
         }
     }
 

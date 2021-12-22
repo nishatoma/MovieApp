53c10bd480a5683514ec81fda4205886a0190850
nishatoma <nishatoma@gmail.com>
2021-12-21 22:44:02 -0500

    finish app

--- app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
+++ app/src/main/java/com/example/movieapp/screens/details/DetailsScreen.kt
@@ -1,7 +1,11 @@
 package com.example.movieapp.screens.details
 
+import androidx.compose.animation.ExperimentalAnimationApi
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyRow
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
@@ -10,31 +14,41 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import coil.compose.rememberImagePainter
 import com.example.movieapp.components.MovieAppTopBar
+import com.example.movieapp.components.MovieRow
+import com.example.movieapp.model.Movie
+import com.example.movieapp.model.getMovies
 
+@ExperimentalAnimationApi
 @Composable
 fun DetailsScreen(navController: NavController, movieData: String?) {
 
     // adding the top bar and the back navigation
     MovieAppTopBar(
         padding = 5.dp, topBarText = "Movie Details",
-    navigationIcon = {
-        Icon(
-            imageVector = Icons.Default.ArrowBack,
-            contentDescription = "Arrow Back",
-            Modifier.clickable {
-                navController.popBackStack()
-            }
-        )
-    })
+        navigationIcon = {
+            Icon(
+                imageVector = Icons.Default.ArrowBack,
+                contentDescription = "Arrow Back",
+                Modifier.clickable {
+                    navController.popBackStack()
+                }
+            )
+        })
     {
-            MainContent(navController = navController, movieData = movieData)
+        MainContent(navController = navController, movieData = movieData)
     }
 
 }
 
+@ExperimentalAnimationApi
 @Composable
 fun MainContent(navController: NavController, movieData: String?) {
+    val movie: Movie = getMovies().single { movie ->
+        movieData == movie.id
+    }
+
     Surface(
         modifier = Modifier
             .fillMaxWidth()
@@ -42,9 +56,33 @@ fun MainContent(navController: NavController, movieData: String?) {
     ) {
         Column(
             horizontalAlignment = Alignment.CenterHorizontally,
-            verticalArrangement = Arrangement.Center
+            verticalArrangement = Arrangement.Top
         ) {
-            Text(text = movieData.toString(), style = MaterialTheme.typography.h5)
+            // Add the movie row here
+            MovieRow(movie = movie)
+            Spacer(modifier = Modifier.padding(8.dp))
+            Divider()
+            Text(text = "Movie Images", modifier = Modifier.padding(5.dp))
+            HorizontalScrollableImageView(movie)
+        }
+    }
+}
+
+@Composable
+private fun HorizontalScrollableImageView(movie: Movie) {
+    LazyRow {
+        items(movie.images) { image ->
+            Card(
+                modifier = Modifier
+                    .padding(12.dp)
+                    .size(240.dp),
+                elevation = 5.dp
+            ) {
+                Image(
+                    painter = rememberImagePainter(data = image),
+                    contentDescription = "Movie image"
+                )
+            }
         }
     }
 }
